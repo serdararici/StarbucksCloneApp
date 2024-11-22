2d288535787a49052dfda5b442a54b6db76c51f2
Serdar Arici <serdararici3@gmail.com>
2024-11-22 16:50:17 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/serdararici/starbucks_clone_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.serdararici.starbucks_clone_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/constants/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AColors{
+  AColors._();
+
+  static const Color primaryGreen = Color(0xFF308B64);
+  static const Color secondaryGreen = Color(0xFF32A65A);
+  static const Color darkGreen = Color(0xFF1B724D);
+
+
+  static const Color orange = Color(0xFFBF712C);
+  static const Color lightOrange = Color(0xFFF2C12E);
+
+}
